f64c75c8634679876c44b503839415b33afa44db
PabloSorrentino <psorrentino@frba.utn.edu.ar>
2022-07-14 14:08:41 -0300

    Tests

--- src/main/java/com/dominio/astropay/repositories/PostRepository.java
+++ src/main/java/com/dominio/astropay/repositories/PostRepository.java
@@ -1,11 +1,8 @@
 package com.dominio.astropay.repositories;
 
 import com.dominio.astropay.domain.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
--- src/test/java/com/dominio/astropay/services/impl/PostServiceImplTest.java
+++ src/test/java/com/dominio/astropay/services/impl/PostServiceImplTest.java
@@ -3,45 +3,36 @@ package com.dominio.astropay.services.impl;
 import com.dominio.astropay.api.ApiConsumer;
 import com.dominio.astropay.domain.Post;
 import com.dominio.astropay.repositories.PostRepository;
-import com.dominio.astropay.services.PostService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.mockito.Mockito.when;
 
 @SpringJUnitConfig
 public class PostServiceImplTest {
 
-  @Mock
+  @MockBean
   private PostRepository mockPostRepository;
 
-  @InjectMocks
-  private PostServiceImpl mockPostServiceImpl;
+  @Mock
+  private ApiConsumer apiConsumer;
 
-  private ApiConsumer mockApiConsumer;
+  @InjectMocks
+  private PostServiceImpl postServiceImpl;
 
   @BeforeEach
   public void setUp(){
-    mockPostServiceImpl = new PostServiceImpl();
-    this.mockApiConsumer = Mockito.mock(ApiConsumer.class);
+    postServiceImpl = Mockito.mock(PostServiceImpl.class);
+    when(mockPostRepository.findAll()).thenReturn(createSimplePostsList());
   }
 
-  @Test
-  public void persistPostsTest(){
-    when(mockApiConsumer.getBlogPosts()).thenReturn(this.createSimplePostsList());
-    when(mockPostRepository.saveAll(Mockito.any(List.class))).thenReturn(this.createSimplePostsList());
-    List<Post> postPersisted = mockPostServiceImpl.persistPosts(mockApiConsumer);
-
-    Assertions.assertFalse(postPersisted.isEmpty());
-  }
 
   private List<Post> createSimplePostsList() {
     return Arrays.asList(new Post(), new Post(), new Post());
